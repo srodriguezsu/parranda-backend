de2aedc6e02ccff18f77641eda285ba4a61d5d24
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-07-11 20:31:46 -0500

    minor changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const {login, registro} = require("./controllers/authController");
 const {getAll, update, create, getOne, deleteOne} = require("./controllers/recetaController");
 const {jwtMiddleware} = require("./middlewares/authMiddleware");
+const musicaController = require('./controllers/salonController');
 const app = express();
 const port = 3456;
 
